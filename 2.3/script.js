d0a550acf82d0b122720a06a3ad0840c9c3080a1
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2022-10-04 09:18:17 +0200

    Push unfinished Exercises

---
+++ 2.3/script.js
@@ -0,0 +1,13 @@
+let lebenslauf = {
+    name : "Lucas Blom",
+    alter : 16,
+    beruf : "Student"
+}
+console.log(Object.keys(lebenslauf))
+console.log(Object.values(lebenslauf))
+
+let dont_waste_my_time = lebenslauf.beruf
+console.log(dont_waste_my_time)
+
+let element = element.push({Lebensstatus: "Ledig"})
+lebenslauf.push(element)
